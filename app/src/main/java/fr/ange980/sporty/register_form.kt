79aed31e8080436786229d2901afad1d4fb981d6
ange980 <angelo.abdelmassih@edu.ece.fr>
2023-11-16 13:09:20 +0100

    ajout du register form

---
+++ app/src/main/java/fr/ange980/sporty/register_form.kt
@@ -0,0 +1,11 @@
+package fr.ange980.sporty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register_form : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_form)
+    }
+}
