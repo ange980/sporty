d4af7fcbccc6abfee10c8fef0dad0a9cb288cdc3
ange980 <angelo.abdelmassih@edu.ece.fr>
2023-12-03 16:29:25 +0100

    formulaire d'inscriptions finit

--- app/src/main/java/fr/ange980/sporty/InterestFormActivity.kt
+++ app/src/main/java/fr/ange980/sporty/InterestFormActivity.kt
@@ -13,31 +13,56 @@ class InterestFormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interest_form)
 
-        val sports = listOf("Basket","Foot")
+        val sports = listOf("Basket", "Foot")
 
-        val autoComplete : AutoCompleteTextView= findViewById(R.id.auto_complete)
+        val autoComplete: AutoCompleteTextView = findViewById(R.id.auto_complete)
 
-        val adapter = ArrayAdapter(this,R.layout.list_sport,sports)
+        val adapter = ArrayAdapter(this, R.layout.list_sport, sports)
 
         autoComplete.setAdapter(adapter)
 
-        autoComplete.onItemClickListener= AdapterView.OnItemClickListener {
-            adapterView, view, i, l ->
-            val sportSelected = adapterView.getItemAtPosition(i)
-            Toast.makeText(this, "Sport: $sportSelected", Toast.LENGTH_SHORT).show()
-        }
-        val freq = listOf("Jamais","Parfois","Souvent","Tout les jours")
+        autoComplete.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val sportSelected = adapterView.getItemAtPosition(i)
+                Toast.makeText(this, "Sport: $sportSelected", Toast.LENGTH_SHORT).show()
+            }
+        val autoComplete2: AutoCompleteTextView = findViewById(R.id.auto_complete2)
 
-        val autoComplete1 : AutoCompleteTextView= findViewById(R.id.auto_complete1)
+        val adapter2 = ArrayAdapter(this, R.layout.list_sport, sports)
 
-        val adapter1 = ArrayAdapter(this,R.layout.list_sport,freq)
+        autoComplete2.setAdapter(adapter2)
+
+        autoComplete2.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val sportSelected = adapterView.getItemAtPosition(i)
+                Toast.makeText(this, "Sport: $sportSelected", Toast.LENGTH_SHORT).show()
+            }
+
+        val freq = listOf("Débutant", "Intermédiaire", "Fort", "Pro")
+
+        val autoComplete1: AutoCompleteTextView = findViewById(R.id.auto_complete1)
+
+        val adapter1 = ArrayAdapter(this, R.layout.list_sport, freq)
 
         autoComplete1.setAdapter(adapter1)
 
-        autoComplete.onItemClickListener= AdapterView.OnItemClickListener {
-                adapterView, view, i, l ->
-            val freqSelected = adapterView.getItemAtPosition(i)
-            Toast.makeText(this, "Sport: $freqSelected", Toast.LENGTH_SHORT).show()
-        }
+        autoComplete.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val freqSelected = adapterView.getItemAtPosition(i)
+                Toast.makeText(this, "Niveau: $freqSelected", Toast.LENGTH_SHORT).show()
+            }
+
+
+        val autoComplete3: AutoCompleteTextView = findViewById(R.id.auto_complete3)
+
+        val adapter3 = ArrayAdapter(this, R.layout.list_sport, freq)
+
+        autoComplete3.setAdapter(adapter3)
+
+        autoComplete3.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val freqSelected = adapterView.getItemAtPosition(i)
+                Toast.makeText(this, "Niveau: $freqSelected", Toast.LENGTH_SHORT).show()
+            }
     }
 }
